1535609e84196923c5de013e7965ea99565d6ddf
Sayandeep Basak <sayandeep1998@gmail.com>
2021-05-14 16:19:54 +0530

    Setup user auth, Default roles and admins, modified registration process

---
+++ leave-management/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUser(userManager);
+
+        }
+        private static void SeedUser(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByNameAsync("admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName="admin",
+                    Email="admin@localhost.com"
+                };
+                var result = userManager.CreateAsync(user, "Papai@1998s").Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+            
+
+        }
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Administrator"
+                };
+            var result=roleManager.CreateAsync(role).Result;
+            }
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+                var result=roleManager.CreateAsync(role).Result;
+            }
+
+        }
+    
+    }
+}
+
